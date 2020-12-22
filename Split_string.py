1feba043d8641f86518067399b155246c0c14659
Rituviks <42962288+Rituviks@users.noreply.github.com>
2020-12-22 13:48:09 +0530

    Add files via upload

---
+++ Split_string.py
@@ -0,0 +1,11 @@
+splitString1 = "I am \n happy \n learning \n python."
+print(splitString1)
+print("-"*80)
+splitString2 = "I am \t happy \t learning \t python."
+print(splitString2)
+print("-"*80)
+splitString3 = "I am\
+                happy \
+                learning\
+                python "
+print(splitString3)
